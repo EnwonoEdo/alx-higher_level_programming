e9265822fc767f9d94223b795af34b8e4e402260
EnwonoEdo <promiseedoinyang@gmail.com>
2024-01-19 13:01:03 -0800

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" update state """
+"""Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+import sys
 from model_state import Base, State
-from sys import argv
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
-
-def add_state():
-    """ access database update state """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
-    session.commit()
-    session.close()
 
-if __name__ == "__main__":
-    add_state()
+    session.query(State).filter(State.id == "2").\
+        update({State.name: "New Mexico"})
+    session.commit()
