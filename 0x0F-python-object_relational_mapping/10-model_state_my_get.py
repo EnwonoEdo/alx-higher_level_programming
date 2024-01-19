53a4e4c02a933b1d9e6b459cbe93afbfb0dbf965
EnwonoEdo <promiseedoinyang@gmail.com>
2024-01-19 12:50:28 -0800

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-""" fetch state by input """
+"""Script that lists all objects that contain the a letter from the database
+"""
+import sys
 from model_state import Base, State
-from sys import argv
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
-
-def fetch_states():
-    """ access database print state """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-    state = session.query(State).filter(State.name == argv[4]).first()
-    if state:
-        print(state.id)
-    else:
-        print("Not found")
-    session.close()
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    fetch_states()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like(sys.argv[4]))
+    if states.count() != 1 or not states:
+        print("Not found")
+    else:
+        print("{}".format(states.first().id))
